c9974fd3eafc06bc9afac4fdc11990c459d26e91
vinodkumar8698 <83947511+vinodkumar8698@users.noreply.github.com>
2021-05-31 18:05:57 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+   d = new Date();
+   hourtime = d.getHours();
+   minutetime = d.getMinutes();
+   secondtime = d.getSeconds(); 
+   hourrotation = 30*hourtime + minutetime/2;
+   minuterotation = 6 * minutetime;
+   secondrotation = 6 * secondtime;
+
+   hour.style.transform = `rotate(${hourrotation}deg)`;
+   minute.style.transform = `rotate(${minuterotation}deg)`;
+   second.style.transform = `rotate(${secondrotation}deg)`;
+
+
+}, 1000);
